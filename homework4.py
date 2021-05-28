cea4737f594baad9f93f9c063df5efad779be3ae
Гришанова Мария Сергеевна <mashenka_grishanova@mail.ru>
2021-05-28 20:29:42 +0300

    ДЗ №4

--- homework4.py
+++ homework4.py
@@ -18,7 +18,6 @@ new_list = [el for el in my_list[1:] if el > my_list[my_list.index(el)-1]]
 my_list.pop(0)
 print(new_list)
 
-
 #Задание 3
 print (f'Числа от 20 до 240, кратные 20 или 21 - это {", ".join(map(str, [el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]))}')
 
